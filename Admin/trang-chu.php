69fee9f7c29eea3166f1168e1724891fe4c1face
Cao Xuân Thắng <114093877+caoxuanthang2810@users.noreply.github.com>
2022-11-18 02:10:57 +0700

    up code main

---
+++ Admin/trang-chu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mx-auto">
+
+        <div class="bg-white w-full h-[676px] mt-10 rounded-xl text-center pt-4 ">
+    
+            <h1 class="text-5xl text-black ">ĐÂY LÀ TRANG CHỦ</h1>
+        </div>
+    </div>
+</body>
+</html>
