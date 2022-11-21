5c6a9f80e80845f1634af67b3dd59d61d3e5edbe
TriChua18 <trintph20275@fpt.edu.vn>
2022-11-21 14:51:38 +0700

    Up code Bảo hành

--- modules/admin/insurance/views/indexView.php
+++ modules/admin/insurance/views/indexView.php
@@ -1,60 +1,56 @@
 <?php get_header('', 'Bảo hành') ?>
 
-    <!--begin::Subheader-->
-    <div class="bg-gray-300 pt-2">
+<!--begin::Subheader-->
+<div class="bg-gray-300 pt-2">
     <div class=" container mx-auto  ">
         <div class="ml-[180px] w-5/6 mt-[-510px]">
+            <form action="" class="flex my-[20px] justify-between">
+                <div>
+                </div>
+                <div>
+                    <div class="w-[150px] h-[50px] bg-amber-400  ml-[20px] rounded-[5px] text-center">
+                        <a href="?role=admin&mod=insurance&action=create" class="">
+
+                            <span class="text-white text-2xl leading-[50px]">
+                                Thêm mới
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </form>
             <div class="bg-slate-100 h-[50px] ">
 
                 <table>
                     <tr class="text-center h-[55px]">
                         <th class="w-[100px]">ID</th>
-                        
+
                         <th class="w-[1250px]">Bảo hành</th>
-                        
+
                         <th class="w-[100px]">Hành động</th>
 
                     </tr>
-                    
-                    <?php foreach ($insurance as $key):
+
+                    <?php foreach ($insurance as $key) :
                         extract($key)
                     ?>
-                    <tr class="text-center h-[55px]">
-                        <td><?=$id?></td>
-                        
-                        <td><?=$name?></td>
-                       
-                        <td><button> <i class="fa-solid fa-pen mr-2"></i></button>
-                            <button><i class="fa-solid fa-trash-can"></i></button>
-                        </td>
-                    </tr>
-                    <?php endforeach;?>
-                </table>
-                <!-- <table>
-      <tr>
-         <th class="ml-[30px]">Tên đơn</th>
-         <th>Giá sản phẩm</th>
-         <th>Số lượng</th>
-         <th>Ảnh sản phẩm</th>
-         <th>Bảo hành</th>
-         <th>Xuất Xứ</th>
-         <th>Mã danh mục</th>
-         <th>Hành động</th>
-      </tr>
-      
-      <tr>
-         <td>1</td>
-         <td>5</td>
-         </tr>
-         <tr>
-         <td>9</td>
-         <td>10</td>
-         </tr>
-      </table> -->
+                        <tr class="text-center h-[55px]">
+                            <td><?=$id?></td>
+                            <td><?= $name_insurance ?></td>
+                            <td>
+                                <a href="?role=admin&mod=insurance&action=update&id=<?=$id?>">
+                                    <button> <i class="fa-solid fa-pen mr-2"></i></button>
+                                </a>
+                                <a href="?role=admin&mod=insurance&action=delete&id=<?=$id?>" onclick="return confirm('Bạn chắc chắn muốn xoá danh mục: <?php echo $row['name_made_in'] ?> không? Hành động sẽ xoá danh mục và toàn bộ sản phẩm có trong danh mục này.')">
+                                    <button><i class="fa-solid fa-trash-can"></i></button>
+                                </a>
 
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
             </div>
         </div>
     </div>
-    </div>
+</div>
 
 <?php get_footer() ?>
--- modules/admin/made_in/views/indexView.php
+++ modules/admin/made_in/views/indexView.php
@@ -4,20 +4,20 @@
 <div class="bg-gray-300 pt-2">
     <div class=" container mx-auto  ">
         <div class="ml-[180px] w-5/6 mt-[-510px]">
-        <form action="" class="flex my-[20px] justify-between">
-            <div>
-            </div>
-            <div>
-                <div class="w-[150px] h-[50px] bg-amber-400  ml-[20px] rounded-[5px] text-center">
-                    <a href="?role=admin&mod=made_in&action=create" class="">
-
-                       <span class="text-white text-2xl leading-[50px]">
-                       Thêm mới
-                       </span>
-                    </a>
+            <form action="" class="flex my-[20px] justify-between">
+                <div>
                 </div>
-            </div>
-        </form>
+                <div>
+                    <div class="w-[150px] h-[50px] bg-amber-400  ml-[20px] rounded-[5px] text-center">
+                        <a href="?role=admin&mod=made_in&action=create" class="">
+
+                            <span class="text-white text-2xl leading-[50px]">
+                                Thêm mới
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </form>
             <div class="bg-slate-100 h-[50px] ">
 
                 <table>
@@ -30,14 +30,14 @@
 
                     </tr>
                     <?php
-                    foreach ($made_in as $row) :?>
+                    foreach ($made_in as $row) : ?>
 
-                    <tr class="text-center h-[55px]">
-                        <td><?=$row['id']?></td>
+                        <tr class="text-center h-[55px]">
+                            <td><?= $row['id'] ?></td>
 
-                        <td><?=$row['name_made_in']?></td>
+                            <td><?= $row['name_made_in'] ?></td>
 
-                        <td>
+                            <td>
                                 <a href="?role=admin&mod=made_in&action=update&id=<?php echo $row['id'] ?>">
                                     <button> <i class="fa-solid fa-pen mr-2"></i></button>
                                 </a>
@@ -46,10 +46,10 @@
                                 </a>
 
                             </td>
-                    </tr>
+                        </tr>
                     <?php
                     endforeach;
-                   ?>
+                    ?>
                 </table>
 
 
