7006df97be6c42a31bcf9347e791c21f8b11f1d7
TriChua18 <trintph20275@fpt.edu.vn>
2022-11-21 14:59:14 +0700

    Update header_admin.php

--- layout/header_admin.php
+++ layout/header_admin.php
@@ -7,12 +7,16 @@
 
 <div class="h-[50px] flex justify-between bg-[#F29D38] leading-[50px]">
 	<div class="ml-[250px]">
-		<a href="?role=client&mod=home">
+		<a href="?role=admin&mod=home">
 			<i class="fa-solid fa-arrow-left text-white"></i>
 			<span class="text-white font-bold">TRANG CHỦ</span>
 		</a>
 	</div>
 	<div class="mr-10">
+		<a href="?role=client&mod=home" class="mr-5">
+			<i class="fa-solid fa-house text-white"></i>
+			<span class="text-white font-bold">VỀ WEBSITE</span>
+		</a>
 		<a href="">
 			<i class="fa-solid fa-arrow-right-to-bracket text-white"></i>
 			<span class="text-white font-bold">ĐĂNG XUẤT</span>
@@ -27,7 +31,7 @@
 
 	<div class=" ">
 		<div class="w-[250px]  bg-white">
-			
+
 			<div class="">
 				<p class="h-[30px] mt-5"><a href="?role=admin&mod=home" class="text-lg ml-4 text-medium text-black hover:text-[#EFA969]">Trang Chủ</a></p>
 				<p class="h-[30px] mt-5"><a href="?role=admin&mod=users" class="text-lg ml-4 text-medium text-black hover:text-[#EFA969]">Quản Lý Tài Khoản</a></p>
