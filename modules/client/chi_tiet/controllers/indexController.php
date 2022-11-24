615681ba7e4678f1b0df3df5eba54aa00b0478ac
TriChua18 <trintph20275@fpt.edu.vn>
2022-11-24 20:22:18 +0700

    UP code

--- modules/client/chi_tiet/controllers/indexController.php
+++ modules/client/chi_tiet/controllers/indexController.php
@@ -8,11 +8,11 @@ function indexAction() {
     $id = $_GET['id'];
     $cate = get_one_products($id);
     $data["products"] = $cate;
-    foreach($data as $key){
-        // extract($data)
-        $id_cate = $key["id_categories"];
-        $data['categories'] = get_categories_by_id($id_cate);
-    }
+    // foreach($data as $key){
+    //     // extract($data)
+    //     $id_cate = $key["id_categories"];
+    //     $data['categories'] = get_categories_by_id($id_cate);
+    // }
     load_view('index', $data);
 }
 
